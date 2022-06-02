9420e4217dcb8f72c5242e1990d6404b92f8daf8
Rashomon <mohammad18018@mail.unpad.ac.id>
2022-06-02 10:58:18 +0700

    update closing

--- resources/views/pages/event/closing.blade.php
+++ resources/views/pages/event/closing.blade.php
@@ -1,18 +1,27 @@
-@extends('layout.temp') 
-@section('title','Epicentrum - Closing Event') 
-@section('content') 
+@extends('layout.temp')
+@section('title','Epicentrum - Closing Event')
+@section('content')
 <!-- ======= Hero Section ======= -->
 <section id="hero" class="d-flex align-items-center justify-content-center ">
-<div class="container" data-aos="fade-up">
-	<div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
-		<div class="col-xl-7 col-lg-8 col">
-			<h1 class="comp_title">Closing Event</h1>
-			<h2></h2>
+	<div class="container" data-aos="fade-up">
+		<div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
+			<div class="col-xl-7 col-lg-8 col">
+				<h1 class="comp_title">Closing Event</h1>
+				<h2></h2>
+			</div>
 		</div>
-    </div>
-</div>
+	</div>
 </section>
 <section class="d-flex align-items-center justify-content-center">
+	<div class="container" data-aos="fade-up">
+		<div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
+			<div class="col-xl-7 col-lg-8" style="background: #637fa8">
+				<img src="{{ url('assets/img/poster_closing.png')}}" class="img-fluid">
+			</div>
+		</div>
+	</div>
+</section>
+<!-- <section class="d-flex align-items-center justify-content-center">
 <div class="container" data-aos="fade-up">
 	<div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
 		<div class="col-xl-12 col-lg-12">
@@ -20,36 +29,27 @@
 		</div>
     </div>
 </div>
-</section>
-<!--<section class="d-flex align-items-center justify-content-center">
-<div class="container" data-aos="fade-up">
-	<div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
-		<div class="col-xl-7 col-lg-8"  style="background: #637fa8">
-			<img src="{{ url('assets/img/pre_event_poster.jpg')}}" class="img-fluid">
-		</div>
-    </div>
-</div>
-</section>-->
-
+</section> -->
+<!-- 
 <section id="hero" class="d-flex align-items-center justify-content-center ">
-<div class="container" data-aos="fade-up">
-	<div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
-		<div class="col-xl-7 col-lg-8 col">
-			<h1 class="comp_title">Guest Star</h1>
-			<h2></h2>
+	<div class="container" data-aos="fade-up">
+		<div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
+			<div class="col-xl-7 col-lg-8 col">
+				<h1 class="comp_title">Guest Star</h1>
+				<h2></h2>
+			</div>
 		</div>
-    </div>
-</div>
+	</div>
 </section>
 <section class="d-flex align-items-center justify-content-center">
-<div class="container" data-aos="fade-up">
-	<div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
-		<div class="col-xl-12 col-lg-12">
-			<p class="coming_soon"><b>Coming Soon</b></p>
+	<div class="container" data-aos="fade-up">
+		<div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
+			<div class="col-xl-12 col-lg-12">
+				<p class="coming_soon"><b>Coming Soon</b></p>
+			</div>
 		</div>
-    </div>
-</div>
-</section>
+	</div>
+</section> -->
 <!--<section class="d-flex align-items-center justify-content-center">
 <div class="container" data-aos="fade-up">
 	<div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
@@ -61,24 +61,53 @@
 </section>-->
 
 <section class="d-flex align-items-center justify-content-center ">
-<div class="container" data-aos="fade-up">
-	<div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
-		<div class="col-xl-7 col-lg-8 col">
-			<h1 class="comp_title_li">Documentation</h1>
-			<h2></h2>
+	<div class="container" data-aos="fade-up">
+		<div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
+			<div class="col-xl-7 col-lg-8 col">
+				<h1 class="comp_title_li">Documentation</h1>
+				<h2></h2>
+			</div>
 		</div>
-    </div>
-</div>
+	</div>
 </section>
-<section class="d-flex align-items-center justify-content-center">
-<div class="container" data-aos="fade-up">
-	<div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
-		<div class="col-xl-12 col-lg-12">
-			<p class="coming_soon"><b>Coming Soon</b></p>
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="column_2">
+				<img src="{{ url('assets/img/closing/1.jpg')}}" style="width:100%">
+				<img src="{{ url('assets/img/closing/2.jpeg')}}" style="width:100%">
+				<img src="{{ url('assets/img/closing/3.jpeg')}}" style="width:100%">
+				<img src="{{ url('assets/img/closing/15.PNG')}}" style="width:100%">
+			</div>
+			<div class="column_2">
+				<img src="{{ url('assets/img/closing/4.jpeg')}}" style="width:100%">
+				<img src="{{ url('assets/img/closing/5.jpeg')}}" style="width:100%">
+				<img src="{{ url('assets/img/closing/6.PNG')}}" style="width:100%">
+			</div>
+			<div class="column_2">
+				<img src="{{ url('assets/img/closing/7.PNG')}}" style="width:100%">
+				<img src="{{ url('assets/img/closing/8.PNG')}}" style="width:100%">
+				<img src="{{ url('assets/img/closing/9.PNG')}}" style="width:100%">
+				<img src="{{ url('assets/img/closing/13.PNG')}}" style="width:100%">
+			</div>
+			<div class="column_2">
+				<img src="{{ url('assets/img/closing/10.PNG')}}" style="width:100%">
+				<img src="{{ url('assets/img/closing/11.PNG')}}" style="width:100%">
+				<img src="{{ url('assets/img/closing/12.PNG')}}" style="width:100%">
+				<img src="{{ url('assets/img/closing/14.PNG')}}" style="width:100%">
+			</div>
 		</div>
-    </div>
-</div>
+	</div>
 </section>
+<!-- <section class="d-flex align-items-center justify-content-center">
+	<div class="container" data-aos="fade-up">
+		<div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
+			<div class="col-xl-12 col-lg-12">
+				<p class="coming_soon"><b>Coming Soon</b></p>
+			</div>
+		</div>
+	</div>
+</section> -->
 <!--<section class="d-flex align-items-center justify-content-center">
 <div class="container" data-aos="fade-up">
 	<div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
